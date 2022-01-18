df18bc8e0922be0825b8a1f19ec3e6bede0373a4
Luis Naranjo <luisnaranjo733@gmail.com>
2022-01-17 16:13:47 -0800

    augment tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -43,15 +43,70 @@ describe("Tiramisu savings club", () => {
     }
   }
 
-  test("All accounts deposit and first account withdraws max", async () => {
-    await allAccountsDeposit(100);  
+  test("Full round of deposits and withdrawals - golden path", async () => {
+    const recurringDepositAmount = 100; // Each member should deposit this much per period
+    const periodsToSimulate = accounts.length; // Pay periods equal to # of addresses to simulate the whole enchilada
 
-    const expectedBalance = accounts.length * 100;
-    expect(await contract.getBalance()).to.equal(expectedBalance);
+    for (let i = 0; i < periodsToSimulate; i++) {
+      await allAccountsDeposit(recurringDepositAmount); // Each account is honest and pays the exact amount they owe on time
+      let nextPayeeIndex = await contract.nextPayee();
+      let nextPayee = accounts[nextPayeeIndex];
+  
+      const withdrawalAmount = recurringDepositAmount * accounts.length;
+      const balanceBefore = await contract.getBalance();
+      await contract.connect(nextPayee).withdraw(withdrawalAmount);
+      const balanceAfter = await contract.getBalance();
+      expect(balanceBefore).to.equal(withdrawalAmount);
+      expect(balanceAfter).to.equal(0);
+    }
 
-    await contract.connect(accounts[0]).withdraw(expectedBalance);
+    // Confirm that nextPayee cycled back to 0 at the end (modulo behavior)
+    expect(await contract.nextPayee()).to.equal(0);
+  });
 
-    expect(await contract.getBalance()).to.equal(0);
+  test("One user fails to deposit, payee fails to withdraw owed amount", async () => {
+    const recurringDepositAmount = 100; // Each member should deposit this much per period
+
+    // All users except the last user pay their dues on time - we expect sc account balance to be underfunded
+    for (let i = 0; i < accounts.length - 1; i++) {
+      await contract.connect(accounts[i]).deposit({ value: recurringDepositAmount });
+    }
+
+    let nextPayeeIndex = await contract.nextPayee();
+    expect(nextPayeeIndex).to.equal(0);
+    let nextPayee = accounts[nextPayeeIndex];
+
+    const balanceBefore = await contract.getBalance();
+    expect(balanceBefore).to.equal(recurringDepositAmount * (accounts.length - 1));
+
+    // Should throw an exception, transaction should be reverted
+    // Smart contract account is underfunded, can't actually withdrew expected amount
+    async function fails() {
+      await contract.connect(nextPayee).withdraw(recurringDepositAmount * accounts.length);
+    }
+
+    await expect(fails()).to.be.rejectedWith(Error, "Cannot withdraw more than the current balance");
+
+    const balanceAfter = await contract.getBalance();
+    expect(balanceAfter).to.equal(balanceBefore); // balance stays the same b/c transaction failed
+
+    // Confirm that nextPayee was not incremented b/c transaction failed
+    expect(await contract.nextPayee()).to.equal(nextPayeeIndex);
+
+    // Payee might as well withdraw the remaining balance, even if it is less than they were owed
+    await contract.connect(nextPayee).withdraw(balanceBefore);
+    expect(await contract.getBalance()).to.equal(0); // smart contract account balance should be drained
+
+    const payeeDeposits = await contract.deposits(nextPayee.address);
+    const payeeWithdrawals = await contract.withdrawals(nextPayee.address);
+    expect(payeeDeposits).to.equal(recurringDepositAmount); // Payee did their part, and paid their dues
+    expect(payeeWithdrawals).to.equal(balanceBefore); // Payee received this much
+    // This user's owed balance should be negative, even though they got screwed by the other member who didn't pay
+    // Owed balance should be (-1) * (recurringDepositAmount * (accounts.length - 2))
+    // 2 payments are missing, one because they got screwed by one of their peers and never got that portion of the payout
+    // the other because this payee did pay their dues for this round
+    expect(payeeDeposits - payeeWithdrawals).to.equal(-recurringDepositAmount * (accounts.length - 2)); // 2 parts missing, one from bad egg who didn't pay their 
+
   });
 
   test("All accounts deposit and first account fails to overdraft the account", async () => {
