4405873a53803be32d52f5c84d493515fcb174a8
Luis Naranjo <luisnaranjo733@gmail.com>
2022-01-22 08:00:54 -0800

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    env: {
-      browser: false,
-      es2021: true,
-      mocha: true,
-      node: true,
-    },
-    plugins: ["@typescript-eslint"],
-    extends: [
-      "standard",
-      "plugin:prettier/recommended",
-      "plugin:node/recommended",
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
     ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    rules: {
-      "node/no-unsupported-features/es-syntax": [
-        "error",
-        { ignores: ["modules"] },
-      ],
-    },
-  };
-  
+    "node/no-unpublished-require": "off",
+    "node/no-unpublished-import": "off",
+  },
+};
