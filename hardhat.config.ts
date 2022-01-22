54270f5fc0f0c46da34b8b95c4d644cd33b826db
Luis Naranjo <luisnaranjo733@gmail.com>
2022-01-22 08:44:25 -0800

    Update env vars

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import * as dotenv from "dotenv";
+import 'dotenv/config'
 
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
@@ -7,7 +7,6 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-dotenv.config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
@@ -30,8 +29,8 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
